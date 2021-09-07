2c43c26d9be32570902bc166859e443a2852c3f1
Alfredo Sánchez <alfred3ks@gmail.com>
2021-09-07 10:30:09 +0200

    Acabo header mobile.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// obtenemos boton y lista
+const button = document.getElementById('button');
+const list= document.getElementById('list');
+const ul = document.getElementById('list__hide');
+
+const change = ()=>{
+    button.classList.toggle('change')
+    list.classList.toggle('change__menu')
+}
+
+button.addEventListener('click', change);
+ul.addEventListener('click', change);
