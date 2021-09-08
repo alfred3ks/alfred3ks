a02efc36427f2cf1075ada61f9d37045195e6dff
Alfredo Sánchez <alfred3ks@gmail.com>
2021-09-08 19:46:35 +0200

    Agrego el boton de ir arriba al hacer scroll.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const button = document.getElementById('button');
 const list= document.getElementById('list');
 const ul = document.getElementById('list__hide');
+const button_up = document.getElementById('button__up');
 
 const change = ()=>{
     button.classList.toggle('change')
@@ -10,3 +11,36 @@ const change = ()=>{
 
 button.addEventListener('click', change);
 ul.addEventListener('click', change);
+
+
+// Botton volver arriba
+// agregamos los estilos al boton de volver
+const add = ()=>{
+    button_up.classList.add('show');
+}
+
+// Le quitamos los estilos
+const remove = ()=>{
+    button_up.classList.remove('show');
+}
+
+const volverUp = ()=>{
+    console.log('click');
+    window.scrollTo({
+        top:0,
+        behavior:'smooth'
+    })
+}
+
+// Hacemos aparecer el boton al scroll de 100
+window.onscroll = function(){
+    const scroll = document.documentElement.scrollTop;
+    console.log(scroll);
+    if(scroll > 100){
+        add();
+    } else {
+    remove();
+        }
+}
+
+button_up.addEventListener('click', volverUp);
