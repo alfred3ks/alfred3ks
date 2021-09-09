01000ce9eb6c3c136e54c84ed0507fe51a9ae67c
Alfredo Sánchez <alfred3ks@gmail.com>
2021-09-09 18:46:36 +0200

    Termino la seccions about en mobile.

--- js/app.js
+++ js/app.js
@@ -12,35 +12,3 @@ const change = ()=>{
 button.addEventListener('click', change);
 ul.addEventListener('click', change);
 
-// Botton volver arriba
-// Agrego los estilos al boton de volver
-const add = ()=>{
-    button_up.classList.add('show');
-}
-
-// Le quito los estilos
-const remove = ()=>{
-    button_up.classList.remove('show');
-}
-
-// Creo la funcion para que se haga scroll al inicio
-const volverUp = ()=>{
-    console.log('click');
-    window.scrollTo({
-        top:0,
-        behavior:'smooth'
-    })
-}
-
-// Hacemos aparecer el boton al scroll de 100
-window.onscroll = function(){
-    const scroll = document.documentElement.scrollTop;
-    console.log(scroll);
-    if(scroll > 100){
-        add();
-    } else {
-    remove();
-        }
-}
-
-button_up.addEventListener('click', volverUp);
