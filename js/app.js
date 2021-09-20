0099c03ff880f62282306071c50e45cfa35f88cc
Alfredo Sánchez <alfred3ks@gmail.com>
2021-09-20 15:30:22 +0200

    Agrego en app las funciones que hacen scroll al hacer click en el menu.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ const button = document.getElementById('button');
 const list= document.getElementById('list');
 const ul = document.getElementById('list__hide');
 
+const about = document.getElementById('about');
+const projects = document.getElementById('projects');
+
+
 const change = ()=>{
     button.classList.toggle('change')
     list.classList.toggle('change__menu')
@@ -11,3 +15,29 @@ const change = ()=>{
 button.addEventListener('click', change);
 ul.addEventListener('click', change);
 
+// Funcion al hacer click en el menu
+
+window.onscroll = function(){
+    const scroll = document.documentElement.scrollTop;
+    console.log(scroll);
+}
+
+// Ir a sobre mi
+const sobreMi = ()=>{
+        window.scrollTo({
+        top:0,
+        behavior:'smooth'
+    })
+}
+
+about.addEventListener('click', sobreMi);
+
+// Ir a proyectos
+const irProjects = ()=>{
+        window.scrollTo({
+        top:548,
+        behavior:'smooth'
+    })
+}
+
+projects.addEventListener('click', irProjects);
